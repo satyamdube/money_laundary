df2e7601f60763324fe64f69fb2e5490bacf9881
satyam dubey <75192962+satyamdube@users.noreply.github.com>
2024-12-23 01:28:05 +0530

    ui update

--- src/header/index.js
+++ src/header/index.js
@@ -14,9 +14,6 @@ const Header = () => {
 		</div>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container">
-            <a className="navbar-brand" href="/">
-              <img src="./images/logo.png" alt="logo"/>
-            </a>
             <button 
               className="navbar-toggler" 
               type="button" 
@@ -29,7 +26,7 @@ const Header = () => {
             <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
-              <ul className="navbar-nav ml-auto action-buttons">
+              <ul className="navbar-nav action-buttons">
                 <li className="nav-item">
                   <a className="nav-link active" aria-current="page" href="/">Home</a>
                 </li>
@@ -50,11 +47,11 @@ const Header = () => {
                   <a className="nav-link" href="#">About</a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#">What do we do</a>
-                </li>
-                <li className="nav-item">
                   <a className="nav-link" href="#">How We Work</a>
                 </li>
+                <li className="nav-item"><a className="navbar-brand" href="/">
+                  <img src="./images/logo.png" alt="logo"/>
+                </a></li>
                 <li className="nav-item">
                   <a className="nav-link" href="#">Our story</a>
                 </li>
@@ -63,6 +60,9 @@ const Header = () => {
                 </li>
                 <li className="nav-item">
                   <a className="nav-link" href="#">Price</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link btn-style" href="#">Book Now</a>
                 </li>
               </ul>
             </div>
