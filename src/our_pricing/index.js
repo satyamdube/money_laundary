cf5b254b0c2b38f061b7930b322ced6e158ada41
satyam dubey <75192962+satyamdube@users.noreply.github.com>
2025-01-04 19:32:36 +0530

    pricing page

--- src/our_pricing/index.js
+++ src/our_pricing/index.js
@@ -6,28 +6,263 @@ const OurPricing = () => {
   return (
     <div>
 		<Header/>
-		<div className="bannerInfoData">
-			<div className="logoInfoDataNew">
-				<img src="./images/logoInfo.png" alt="logo banner"/>
-			</div>
-		</div>
-		<div className="mightInfoData">
-			<div className="container">
-				<div className="innerDataCard">
-					<p>sds ABLE TO WASH YOUR MONEY, BUT WE SURE CAN WASH YOUR CLOTHES LIKE ITS MONEY.</p>
-					<div className="moneyGIf">
-						<img src="./images/money.gif" alt="money"/>
+		  <div className="pricingBannerInfo">
+			  <div className="container">
+				 <div className="pricingBnr">
+					<h2>OUR <br/> PRICING</h2>
+					<p>SAVE 25% ON YOUR FIRST ORDER WITH CODE TML25X</p>
+					<div className="browswPrices">
+						<a href="#offer">Browse our prices</a>
+						<div className="downArrow"><img src="./images/downArrow.png"/></div>
 					</div>
-					<span className="linerData"></span>
-					<div className="bookInfo">
-					   <a href=""> <img src="./images/bookInfo.png" alt="logo banner"/></a>
+				 </div>
+			  </div>
+			  <div className="pricedataManin">
+				 <div className="container">
+					<div className="offerInfo" id="offer">
+						<h3>Offers</h3>
+						<div className="cardPriceInfo">
+							<div className="cardDataNew">
+								<div className="leftCardNew">
+									<table>
+										<tr><td className="bgData">7%</td><td>749/-</td></tr>
+									</table>
+								</div>
+								<div className="leftCardNew">
+									<table>
+										<tr><td colSpan={"2"}>2 Suits Deal (trouser or skirt + jacket)<p>(Usual Price 799/-)</p></td></tr>
+									</table>
+								</div>
+							</div>
+							<div className="cardDataNew">
+								<div className="leftCardNew">
+									<table>
+										<tr><td className="bgData">7%</td><td>749/-</td></tr>
+									</table>
+								</div>
+								<div className="leftCardNew">
+									<table>
+										<tr><td colSpan={"2"}>2 Suits Deal (trouser or skirt + jacket)<p>(Usual Price 799/-)</p></td></tr>
+									</table>
+								</div>
+							</div>
+							<div className="cardDataNew">
+								<div className="leftCardNew">
+									<table>
+										<tr><td className="bgData">7%</td><td>749/-</td></tr>
+									</table>
+								</div>
+								<div className="leftCardNew">
+									<table>
+										<tr><td colSpan={"2"}>2 Suits Deal (trouser or skirt + jacket)<p>(Usual Price 799/-)</p></td></tr>
+									</table>
+								</div>
+							</div>
+							<div className="cardDataNew">
+								<div className="leftCardNew">
+									<table>
+										<tr><td className="bgData">7%</td><td>749/-</td></tr>
+									</table>
+								</div>
+								<div className="leftCardNew">
+									<table>
+										<tr><td colSpan={"2"}>2 Suits Deal (trouser or skirt + jacket)<p>(Usual Price 799/-)</p></td></tr>
+									</table>
+								</div>
+							</div>
+							<div className="cntdatanew">
+								<p>Together, Jai and Ashish are committed to redefining fabric care in India, providing a transparent, efficient, and world-class service experience.</p>
+							</div>
+							<div className="IndivusualInfo">
+								<h3>INDIVIDUAL GARMENTS</h3>
+								<p>( COST PER PIECE )</p>
+								<table>
+									<tr>
+										<td>Items</td>
+										<td>Cleaning and Stream Press</td>
+									</tr>
+									<tr>
+										<td>Shirt</td>
+										<td>79 Rs</td>
+									</tr>
+									<tr>
+										<td>Shirt</td>
+										<td>79 Rs</td>
+									</tr>
+									<tr>
+										<td>Shirt</td>
+										<td>79 Rs</td>
+									</tr>
+									<tr>
+										<td>Shirt</td>
+										<td>79 Rs</td>
+									</tr>
+									<tr>
+										<td>Shirt</td>
+										<td>79 Rs</td>
+									</tr>
+									<tr>
+										<td>Shirt</td>
+										<td>79 Rs</td>
+									</tr>
+									<tr>
+										<td>Shirt</td>
+										<td>79 Rs</td>
+									</tr>
+									<tr>
+										<td>Shirt</td>
+										<td>79 Rs</td>
+									</tr>
+									<tr>
+										<td>Shirt</td>
+										<td>79 Rs</td>
+									</tr>
+									<tr>
+										<td>Shirt</td>
+										<td>79 Rs</td>
+									</tr>
+									<tr>
+										<td>Shirt</td>
+										<td>79 Rs</td>
+									</tr>
+									<tr>
+										<td>Shirt</td>
+										<td>79 Rs</td>
+									</tr>
+									<tr>
+										<td>Shirt</td>
+										<td>79 Rs</td>
+									</tr>
+									<tr>
+										<td>Shirt</td>
+										<td>79 Rs</td>
+									</tr>
+									<tr>
+										<td>Shirt</td>
+										<td>79 Rs</td>
+									</tr>
+									<tr>
+										<td>Shirt</td>
+										<td>79 Rs</td>
+									</tr>
+									<tr>
+										<td>Shirt</td>
+										<td>79 Rs</td>
+									</tr>
+									<tr>
+										<td>Shirt</td>
+										<td>79 Rs</td>
+									</tr>
+									<tr>
+										<td>Shirt</td>
+										<td>79 Rs</td>
+									</tr>
+									<tr>
+										<td>Shirt</td>
+										<td>79 Rs</td>
+									</tr>
+									<tr>
+										<td>Shirt</td>
+										<td>79 Rs</td>
+									</tr>
+									<tr>
+										<td>Shirt</td>
+										<td>79 Rs</td>
+									</tr>
+									<tr>
+										<td>Shirt</td>
+										<td>79 Rs</td>
+									</tr>
+									<tr>
+										<td>Shirt</td>
+										<td>79 Rs</td>
+									</tr>
+									<tr>
+										<td>Shirt</td>
+										<td>79 Rs</td>
+									</tr>
+								</table>
+							</div>
+							<div className="IndivusualInfo">
+								<h3>INDIVIDUAL GARMENTS</h3>
+								<p>( COST PER PIECE )</p>
+								<table>
+									<tr>
+										<td>Items</td>
+										<td>Cleaning and Stream Press</td>
+									</tr>
+									<tr>
+										<td>Shirt</td>
+										<td>79 Rs</td>
+									</tr>
+									<tr>
+										<td>Shirt</td>
+										<td>79 Rs</td>
+									</tr>
+									<tr>
+										<td>Shirt</td>
+										<td>79 Rs</td>
+									</tr>
+									<tr>
+										<td>Shirt</td>
+										<td>79 Rs</td>
+									</tr>
+									<tr>
+										<td>Shirt</td>
+										<td>79 Rs</td>
+									</tr>
+								</table>
+							</div>
+							<div className="IndivusualInfo">
+								<h3>INDIVIDUAL GARMENTS</h3>
+								<br/><br/>
+								<table>
+									<tr>
+										<td>Items</td>
+										<td>Cleaning and Stream Press</td>
+									</tr>
+									<tr>
+										<td>Shirt</td>
+										<td>79 Rs</td>
+									</tr>
+									<tr>
+										<td>Shirt</td>
+										<td>79 Rs</td>
+									</tr>
+									<tr>
+										<td>Shirt</td>
+										<td>79 Rs</td>
+									</tr>
+									<tr>
+										<td>Shirt</td>
+										<td>79 Rs</td>
+									</tr>
+									<tr>
+										<td>Shirt</td>
+										<td>79 Rs</td>
+									</tr>
+									<tr>
+										<td>Shirt</td>
+										<td>79 Rs</td>
+									</tr>
+									<tr>
+										<td>Shirt</td>
+										<td>79 Rs</td>
+									</tr>
+								</table>
+							</div>
+						</div>
 					</div>
-				</div>
-			</div>
-		</div>
-		<div className="howItWork">
-		    <img src="./images/howItWork.jpg" alt="How it work"/>
-		</div>
+					<div className="bookNowInfo"><a class="" to="/book_now" data-discover="true"><img alt="Book Now" src="./images/bookNow.png"/></a></div>
+					<div className="tearmsCondition">
+						<h4>Terms & Conditions:-</h4>
+						<p>-Minimum Order Value: -A minimum order of ₹650 is required for all services. </p>
+						<p>-White Garment Surcharge: -An additional charge of ₹20 per white garment applies for specialized care and treatment.</p>
+						<p> -GST Applicable: -All listed prices are exclusive of 18% GST, which will be added to the final bill.</p>
+					</div>
+				 </div>
+			  </div>
+		  </div>
 		<Footer/>
     </div>
   );
